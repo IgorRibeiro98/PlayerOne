a7c0c5ce29ae19a63824cf54feb9d7c52f0a789a
Igor Ribeiro <igor.ribeiro98@outlook.com>
2020-12-01 17:18:18 -0300

    Atualização de recursos

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function criaCard(data){
 
     data.forEach((data) => {
-        console.log(data)
-        console.log(data.img1)
+        //console.log(data)
+        //console.log(data.img1)
         
         let rowCard =  document.getElementById('rowCard');
 
@@ -22,7 +22,7 @@ function criaCard(data){
         let textCardBody = document.createElement('a');
         textCardBody.setAttribute('class', 'btn btn-custom btn-branco btn-block');
         textCardBody.setAttribute('data-toggle', 'modal');
-        textCardBody.setAttribute('data-target', '#dmc');
+        textCardBody.setAttribute('data-target', `#jogo${data.id}`);
         textCardBody.innerText = 'Saiba mais sobre ' + data.titulo;
 
 
@@ -36,4 +36,50 @@ function criaCard(data){
         
     })
 
+    criaModal(data)
+
+}
+
+function criaModal (data){
+
+    data.forEach((data) => {
+        let divModal = document.createElement('div')
+        divModal.setAttribute('id', `jogo${data.id}`)
+        divModal.setAttribute('class', 'modal fade')
+        
+        let divModalDialog = document.createElement('div')
+        divModalDialog.setAttribute('class', 'modal-dialog modal-dialog-centered modal-xl')
+
+        let divModalContent = document.createElement('div')
+        divModalContent.setAttribute('class', 'modal-content')
+
+        let divModalHeader = document.createElement('div')
+        divModalHeader.setAttribute('class', 'modal-header')
+
+        let modalTitulo = document.createElement('h2')
+        modalTitulo.setAttribute('class', 'modal-title')
+        modalTitulo.innerText = data.titulo
+
+        let fechaModal = document.createElement('button')
+        fechaModal.setAttribute('class', 'close')
+        fechaModal.setAttribute('data-dismiss', 'modal')
+
+        let spanModal = document.createElement('span')
+        spanModal.innerHTML = '&times;'
+
+
+        fechaModal.appendChild(spanModal)
+
+        divModalHeader.appendChild(modalTitulo)
+        divModalHeader.appendChild(fechaModal)
+
+        divModalContent.appendChild(divModalHeader)
+
+        divModalDialog.appendChild(divModalContent)
+
+        divModal.appendChild(divModalDialog)
+
+        document.getElementById('body').appendChild(divModal)
+    })
+    
 }
